339e2fdf07f77e2f3bf6e4ed1a4ca641a12331a3
Martin Männik <martinm1@tlu.ee>
2018-12-20 18:52:15 +0200

    Almost Done

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Enemy : MonoBehaviour {
+
+    //Members 
+    
+    public float health = 1.0f;
+    public float pointsGiven = 1.0f;
+    public float moveSpeed = 2.0f;
+    private Vector3 initial;
+    private float passedTime = 0.0f;
+    private GameObject player;
+    public NavMeshAgent agent;
+    private float alive = 1.0f;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        initial = transform.position;
+        
+    }
+    // Update is called once per frame
+    void Update () {
+
+        agent.SetDestination(player.transform.position);
+
+
+        if (health <= 0)
+        {
+            DieRespawn();
+        }
+
+    }
+    public void DieRespawn()
+    {
+        //print("Enemy dies " + this.gameObject.name);
+        player.GetComponent<Player>().points += pointsGiven;
+        transform.position = initial;
+        health += 1;
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public float health = 3.0f;
+    public float moveSpeed = 7.0f;
+    public float points = 0.0f;
+    public GameObject bulletSpawner;
+    public GameObject bullet;
+    private Vector3 initial;
+    private GameObject Life;
+    private GameObject Score;
+    public GameObject GUI;
+    public GameObject StartMenu;
+    public bool playing;
+
+    // Use this for initialization
+    void Start () {
+        GUI = GameObject.FindGameObjectWithTag("GUI");
+        Life = GameObject.FindGameObjectWithTag("Life");
+        Score = GameObject.FindGameObjectWithTag("Score");
+        StartMenu = GameObject.FindGameObjectWithTag("StartMenu");
+        initial = transform.position;
+        playing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Player movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //transform.GetChild(0).LookAt(Vector3.forward, Vector3.up);
+            //transform.GetChild(0).RotateAround(transform.position, transform.up, 0);
+            transform.GetChild(0).rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            //transform.GetChild(0).RotateAround(transform.position, transform.up, -90);
+            //if(Input.GetKeyDown(KeyCode.A))
+                transform.GetChild(0).rotation = Quaternion.Euler(0, -90, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            //transform.GetChild(0).RotateAround(transform.position, transform.up, 180);
+            transform.GetChild(0).rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            //transform.GetChild(0).RotateAround(transform.position, transform.up, 90);
+            transform.GetChild(0).rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        if(GUI.activeInHierarchy)
+        {
+            Life.GetComponent<Text>().text = health.ToString();
+            Score.GetComponent<Text>().text = points.ToString();
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            health -= 1.0f;
+            transform.position = initial;
+            /*transform.Translate(initial.position, Space.World);*/
+            if (health == 0)
+            {
+                StartMenu.SetActive(true);
+                print("Player died");
+                Time.timeScale = 0;
+                transform.position = initial;
+                playing = false;
+            }
+
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bullet.transform, bulletSpawner.transform.position, bulletSpawner.transform.rotation);
+    }
+}
